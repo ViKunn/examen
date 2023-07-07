46a13aa67b4cfe49311fa8028d24f1b213635822
ViKunn <alisson.espin@epn.edu.ec>
2023-07-07 16:01:43 -0500

    se eliminan los cambios de juego, se mantienen los cambios esteticos

--- src/EXAMEN.cpp
+++ src/EXAMEN.cpp
@@ -1,12 +1,17 @@
+//falta documentar métodos
+
 #include <iostream>
 #include <unistd.h>
 #include <windows.h>
 #include <stdio.h>
+#include <conio.h>
 #include <string>
 #include "../lib/aeUtility.h"
 using namespace std;
 #define DELAY 55000
 #define ESPERA 1800000
+
+/*Variables para Autenticación de usuario*/
 
 int aeCedula        = 1600864845,
     aeJuegoContador = 0;                //Quizás tenga que cambiar esta variable a una que no sea global
@@ -14,6 +19,18 @@ string aeNombre     = "Alisson Viviana Espin Ortega",
        aeCorreo     = "alisson.espin@epn.edu.ec",
        aeUser,
        aePassword;
+
+
+/*Variables para juego*/
+
+bool aeObsEstaIzq = true;     //false = 0
+enum {aeObservador, aeLobo, aeCaperucita, aeUvas, aeAnchoRio = 20};
+int aeOpcionMenu = -1;        // aeAnchoRio = 20, aeObservador = 0, aeLobo = 1, aeCaperucita = 2, aeUvas = 3;
+
+string aeArrIzq[]   = { "","Lobo", "Cape", "Uvas" },         //el número de caracteres de cada vector es 10
+       aeArrDer[]   = { "", "",     "",     "" },
+       aeActorCruza = "";                                 //Obse[0]  Lobo[1]  Cape[2]  Uvas[3]
+
 
 /*MÉTODOS PARA PASAR A MAYÚSCULAS O MINÚSCULAS LOS DATOS DEL ESTUDIANTE*/
 
@@ -45,15 +62,30 @@ void aeDatosEstudiante()
 
 /*MÉTODOS PARA VALIDACIÓN DE CREDENCIALES*/
 
+string aeGetPassword(const string aeEtiqueta)
+{
+     char c;
+     string aePassword;
+     cout << aeEtiqueta;
+     aeSetColor(celeste);
+     while((c=getch()) && !(c=='\r'))
+     {
+          aePassword.push_back(c);
+          _putch('*');
+     }
+     return aePassword;
+}
 void aeCredencialesIngresar()
 {
      aeDatosEstudiante();
      cout << aeSetColor(rosado) +   "\t\tINICIO DE SESIÓN"  << "\n\n"
           << aeSetColor(rosado) + "Ingrese su usuario:    "    << aeSetColor(celeste);
      cin  >>  aeUser;
-
-     cout << aeSetColor(rosado) + "Ingrese su contraseña: "  << aeSetColor(celeste) ;
-     cin  >>  aePassword;
+     aeSetColor(rosado);
+     aePassword = aeGetPassword("Ingrese su contraseña: ");
+     cout << endl;
+     // cout << aeSetColor(rosado) + "Ingrese su contraseña: "  << aeSetColor(celeste) ;
+     // cin  >>  aePassword;
 }
 void aeCredencialesValidar()
 {
@@ -107,6 +139,10 @@ void aeCredencialesValidar()
           aeCredencialesContador++;
 
      } while (aeCredencialesContador <= 3);
+
+     // aeConductor = aeUser;
+
+     // return aeConductor;
 }
 
 
@@ -119,75 +155,16 @@ en lugar de "observador" coloque "aeUser", creo que es facil pero tengo que comp
 voy a hacer un commit antes de hacer esto commit - m "Funciones hechas: LOGIN, DATOS DEL ESTUDIANTE, falta JUEGO"
 */
 
-bool aeObsEstaIzq = true;  //false = 0
-int aeOpcionMenu = -1, aeAnchoRio = 20, aeObservador = 0, aeLobo = 1, aeCaperucita = 2, aeUvas = 3;
 
-string aeArrIzq[]   = { aeUser, "   Lobo   ", "Caperucita", "   Uvas   "},     //el número de caracteres de cada vector es 10
-       aeArrDer[]   = {".",     "",     "",     ""},
-       aeActorCruza = "";                                 //Obse[0]  Lobo[1]  Cape[2]  Uvas[3]
-
-string aeShowActor(const string aeArr[])
-{
-     string aeActor = "";
-     for(int i=0; i<4; i++)
-          aeActor += aeArr[i] + ", ";           //concatenar strings
-     return aeActor;
-}
-string aeShowRio(int aeLenRio)
-{
-     string aeRio = "";
-     for (int i = 0; i < aeLenRio-1; i++)
-          aeRio += "_ ";
-     return aeRio;
-}
-void aeMenu()
-{
-     cout << "0 Solo"         << endl
-          << "1 Lobo"         << endl
-          << "2 Caperucita"   << endl
-          << "3 Uvas"         << endl
-          << "4 SALIR"        << endl;
-              
-     do{       //unicamente para escoger el actor no para cambiarlo de string
-          try
-          {
-               string aeStr = "";
-               cout << ">Cruzar: ";
-               cin >> aeStr;                                       //Entrada de datos string y luego validación de datos
-               aeOpcionMenu = stoi(aeStr);                           //string a int "stoi"
-               aeActorCruza = (aeObsEstaIzq) ? aeArrIzq[aeOpcionMenu] : aeArrDer [aeOpcionMenu];      //modifica la variable global de acuerdo a donde está el observador
-               if(aeOpcionMenu == 4)      exit(0);
-               if(aeActorCruza.empty())
-                    throw invalid_argument("No hay actor");      //Se muere el programa
-          //trow No hay actor cuando el string de alguno de los lados está vacío, dependiendo de donde está el observador
-          }
-
-          catch(...)                                             //recupera la muerte del programa y vuelve a intentar, los 3 puntos sirven para decir "no me importa"
-          {
-               aeActorCruza = "";
-               aeOpcionMenu = -1;                                  //se reinicia la opción del menú
-               cout << ":(Opción no válida...) \n";              //cerr es lo mismo que cout
-          }
-     }while(aeOpcionMenu<0);                                       //bucle hasta que la opción seleccionada sea válida
-}
-void aeNavegar()
-{
-     string aeBarca    = "\\_Observador , "+aeActorCruza+"_/";            //concatenación de caracteres
-     string aeActorIzq = aeShowActor(aeArrIzq);
-     string aeActorDer = aeShowActor(aeArrDer);
-
-     for (int i=0; i < aeAnchoRio; i++)
-     {
-          cout << "\r" + aeActorIzq + aeShowRio(i) + aeBarca + aeShowRio(aeAnchoRio - i) + aeActorDer;             //CONCATENAR caracteres
-          usleep(DELAY);
-     }
-}
 
 /*LÓGICA DEL JUEGO*/
 
-void aeJuego()
+/**Se encarga de ejecutar el juego del vikingo que quiere cruzar un rio con un lobo, caperucita y unas uvas*/
+void aeJuegoVikingo()
 {
-
+     /*Aqui dentro se añade la lógica que debería tomar el programa del juego*/
+     cout << endl << aeUser << "\n";
+     cout << endl << "Se ejecuta el juego..." << endl;
 }
 
 /*LÓGICA MAIN DEL PROGRAMA*/
@@ -195,15 +172,49 @@ void aeJuego()
 int main()
 {
      aeClearTerminal();
-     aeCredencialesValidar();
-     // aeUser = "Alisson";
-     for (int i = 0; i < 4; i++)
-     {
-          cout << aeArrIzq[i] << ", ";
-     }
-     cout << endl;
      
-     // aeJuego();
-     cout << endl << "Se ejecuta el juego..." << endl;
+     //actualiza la variable global de aeUser
+     aeCredencialesValidar();
+
+     cout << endl;
+
+     aeJuegoVikingo();
+     
      return 0;
 }
+
+/*
+LÓGICA DEL PROGRAMA SEGUN LA CAPTURA DEL POCO CÓDIGO QUE PUDE VER DEL ING
+
+1. Declarar variables string para ladoIzq y ladoDer y para el actor que cruza
+2. Declarar variable bool para confirmar el estado del observador
+3. Declarar variables int para luego añadir en el menú
+
+     int aeOpcionMenu = -1, aeAnchoRio = 20, aeObservador = 0, aeLobo = 1, aeCaperucita = 2, aeUvas = 3;
+
+4. Crear métodos
+
+     4.1. string aguaRio (int len)
+          va a definir cual es la longitud del río por la que la barca va a pasar
+          sirve para presentar caracteres _ a la izquierda y a la derecha conforme
+          la barca va avanzando
+
+     4.2. string getActores (const string arrActores[])
+          va a funcionar como menú para que el usuario escoja que actor quiere que
+          esté en la barca junto con el al momento de cruzar el río
+
+     4.3. void validarReglas()
+          sirve para determinar si las reglas del juego se están cumpliendo al
+          momento de movilizar a los actores de un lado a otro
+
+     4.4. void showBarcaRio(int 1, const string barca)
+          va a presentar la barca movilizandose de un izquierda a derecha
+
+     4.5. showNavegación()
+          \\_patmic_,_?_/ reemplaza el ? por el actor que está cruzando
+*/
+
+/*   
+LÓGICA DEL PROGRAMA SEGUN COMO YO CREO QUE DEBERÍA FUNCIONAR (pseudocódigo)
+
+*/
